d1dea5fb35b9879cfebd4380e89f8815d3599e06
Nishant-king <nishant@gmail.com>
2026-03-06 21:25:16 +0530

    add dockerfile

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+﻿const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("SHRI AI Agent Running 🚀");
+  res.send("🚀 SHRI AI Agent Running");
 });
 
 const PORT = process.env.PORT || 3000;
